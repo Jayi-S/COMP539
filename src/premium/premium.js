8861211424e9d1f0219c7b62e87aca709dbf7e02
fanggeX <fx8@rice.edu>
2023-12-05 16:19:36 -0600

    plan selection

--- src/premium/premium.js
+++ src/premium/premium.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import './premium.css'; 
+import React, { useState } from 'react';
+import './premium.css';
 
 const Premium = ({ onChoosePlan }) => {
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+  const [selectedPlan, setSelectedPlan] = useState('');
+
+  const handleChoosePlan = (plan) => {
+    setIsPopupVisible(true);
+    setSelectedPlan(plan);
+  };
+
+  const handlePopupResponse = (response) => {
+    setIsPopupVisible(false);
+    if (response === 'yes') {
+      onChoosePlan(selectedPlan);
+    }
+  };
+
   return (
     <div className="premium-plans-container">
       <h2 className="premium-plans-header">Premium Plans</h2>
       <div className="plans">
- 
         <div className="plan basic">
-          <h3>Basic Plan</h3>
+        <h3>Basic Plan</h3>
           <p>Free</p>
           <ul>
             <li>Alias short</li>
@@ -16,12 +30,9 @@ const Premium = ({ onChoosePlan }) => {
             <li>-</li>
             <li>-</li>
             <li>-</li>
-        
           </ul>
-          <button onClick={() => onChoosePlan('basic')}>Choose Plan</button>
+          <button onClick={() => handleChoosePlan('basic')}>Choose Plan</button>
         </div>
-        
-
         <div className="plan standard">
           <h3>Standard Plan</h3>
           <p>$20/month</p>
@@ -33,7 +44,7 @@ const Premium = ({ onChoosePlan }) => {
             <li>-</li>
             
           </ul>
-          <button onClick={() => onChoosePlan('standard')}>Choose Plan</button>
+          <button onClick={() => handleChoosePlan('standard')}>Choose Plan</button>
         </div>
 
 
@@ -48,11 +59,20 @@ const Premium = ({ onChoosePlan }) => {
             <li>24/7 phone and email support</li>
      
           </ul>
-          <button onClick={() => onChoosePlan('premium')}>Choose Plan</button>
+          <button onClick={() => handleChoosePlan('premium')}>Choose Plan</button>
         </div>
       </div>
+
+      {isPopupVisible && (
+        <div className="popup">
+          <p>Do you want to switch to the {selectedPlan} plan?</p>
+          <button onClick={() => handlePopupResponse('yes')}>Yes</button>
+          <button onClick={() => handlePopupResponse('no')}>No</button>
+        </div>
+      )}
     </div>
   );
 };
 
 export default Premium;
+
